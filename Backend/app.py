093b3f0dd0624fcfbc949eb42d1651c46d179c7f
SanjayS598VT <sanjay.shankar598@gmail.com>
2025-03-29 22:36:30 -0400

    GPT API

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,19 @@
-from dotenv import load_dotenv
 import os
-load_dotenv()
+from dotenv import load_dotenv
 from openai import OpenAI
+
+load_dotenv()
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+user_input = input("Enter your query: ")
 
 response = client.responses.create(
     model="gpt-4o",
-    input="Write a one-sentence bedtime story about a unicorn."
+    tools=[{"type": "web_search_preview"}],
+    input=user_input
 )
 
 print(response.output_text)
+
+
